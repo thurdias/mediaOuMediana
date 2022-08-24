7bde47e8ddb08b304fc236740e9f1cf6a30398ec
Arthur Bouças <thurdias@gmail.com>
2022-08-24 16:40:09 -0300

    correções

--- mediaM.java
+++ mediaM.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-																					  // Exercício 2021 - Fase 2 - Média ou mediana
-public class mediaM {                                                                 // Explicação:
-                                                                                      // Método principal com o método do bufferedreader/inputstreamreader
-	                                                                                  // Foi utilizado o bufferedreader por conta dele ser mais rápido, pois ele simplesmente lê a sequência de caracteres, enquanto o scanner faz a análise dos dados de entrada 
-    public static void main(String[] args) throws IOException {                       // IOException é utilizado para evitar erros no sistema de leitura                                                                                                                                     
+																					  // ExercÃ­cio 2021 - Fase 2 - MÃ©dia ou mediana
+public class mediaM {                                                                 // Explicacao:
+                                                                                      // Metodo principal com o metodo do bufferedreader/inputstreamreader
+	                                                                              // Foi utilizado o bufferedreader por conta dele ser mais rÃ¡pido, pois ele simplesmente le a sequencia de caracteres, enquanto o scanner faz a analise dos dados de entrada 
+    public static void main(String[] args) throws IOException {                       // IOException e utilizado para evitar erros no sistema de leitura                                                                                                                                     
     BufferedReader leitor = new BufferedReader(new InputStreamReader(System.in));
-	// Declaração de variáveis
-	int a = Integer.parseInt(leitor.readLine());                                      // Integer.parseInt é utilizado para converter uma string em um int
+	// DeclaraÃ§Ã£o de variÃ¡veis
+	int a = Integer.parseInt(leitor.readLine());                                      // Integer.parseInt e utilizado para converter uma string em um int
 	int b = Integer.parseInt(leitor.readLine());
 
-	// Processamento & Saída
-	if (a < b)                                                                       // A mediana sempre será determinada pelo menor número (A/B) para atender o menor inteiro possível para C
-	    System.out.println(2*a-b);                                                   // explicação matemática: ((a+b+c)/3)=a) {caso A seja menor que b}
-	else                                                                             // igualando os dois lados da equação fica (a=(1/2)*b+(1/2)*c)
-	    System.out.println(2*b-a);                                                   // Como queremos descobrir a variável c, e não podemos declarar meia variável como na expressão anterior
-    }                                                                                // multiplicamos a igualdade por 2 e isolamos o c, ficando (c=(2*a)-b)
+	// Processamento & SaÃ­da
+	if (a < b)                                                                       // A mediana sempre sera determinada pelo menor numero (A/B) para atender o menor inteiro possÃ­vel para C
+	    System.out.println(2*a-b);                                                   // explicaÃ§Ã£o matemÃ¡tica: ((a+b+c)/3)=a) {caso A seja menor que b}
+	else                                                                             // igualando os dois lados da equaÃ§Ã£o fica (a=(1/2)*b+(1/2)*c)
+	    System.out.println(2*b-a);                                                   // Como queremos descobrir a variavel c, e nÃ£o podemos declarar meia variavel como na expressÃ£o anterior
+    }                                                                                    // multiplicamos a igualdade por 2 e isolamos o c, ficando (c=(2*a)-b)
 }
 
